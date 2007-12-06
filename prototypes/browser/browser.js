740456fce48bc3c1b2c081427c52f70f6f8c9872
mattgiuca <>
2007-12-06 22:38:46 +0000

    Added prototypes directory. Added ajax file browser proof-of-concept demo.

---
+++ prototypes/browser/browser.js
@@ -0,0 +1,308 @@
+/* browser.js
+ * Client-side interaction for both file browser and editor.
+ * PROOF-OF-CONCEPT ONLY
+ *
+ * Author: Matt Giuca
+ */
+
+var gpathlist;
+
+/* Removes all children of a given element */
+function removechildren(elem)
+{
+    while (elem.lastChild != null)
+        elem.removeChild(elem.lastChild);
+}
+
+/* Clears all dynamic data from the page, so it can be refilled. */
+function clearfiles()
+{
+    path = document.getElementById("path");
+    removechildren(path);
+    files = document.getElementById("files");
+    removechildren(files);
+}
+
+function makebutton(text, action)
+{
+    button = document.createElement("input");
+    button.setAttribute("type", "button");
+    button.setAttribute("value", text);
+    button.setAttribute("onclick", action);
+    return button;
+}
+
+/* Makes a TD element with text in it */
+function maketd(text)
+{
+    td = document.createElement("td");
+    td.appendChild(document.createTextNode(text));
+    return td;
+}
+
+function maketdwithlink(text, href, onclick)
+{
+    td = document.createElement("td");
+    link = document.createElement("a");
+    link.setAttribute("href", href);
+    if (onclick != null)
+        link.setAttribute("onclick", onclick);
+    link.appendChild(document.createTextNode(text));
+    td.appendChild(link);
+    return td;
+}
+
+function maketdwithactions(filename)
+{
+    td = document.createElement("td");
+    td.appendChild(makebutton("Delete", "rm(\"" + filename + "\")"));
+    td.appendChild(makebutton("Rename", "rename(\"" + filename + "\")"));
+    td.appendChild(makebutton("Edit", "edit(\"" + filename + "\")"));
+    return td;
+}
+
+/* Converts a list of directories into a path name, with a slash at the end */
+function pathlisttopath(pathlist)
+{
+    ret = "";
+    for each (dir in pathlist)
+    {
+        ret += dir + "/";
+    }
+    return ret;
+}
+
+function presentpath(pathlist)
+{
+    path = document.getElementById("path");
+    gpathlist = pathlist;
+    pathlist = ["home"].concat(pathlist);
+    navlist = [];
+    home = true;
+
+    /* Create all of the paths */
+    for each (dir in pathlist)
+    {
+        if (home == false)
+            navlist.push(dir);
+        home = false;
+        /* Make an 'a' element */
+        link = document.createElement("a");
+        link.setAttribute("href", "#");
+        link.setAttribute("onclick", "nav(" + JSON.stringify(navlist) + ")");
+        link.appendChild(document.createTextNode(dir));
+        path.appendChild(link);
+        path.appendChild(document.createTextNode("/"));
+    }
+}
+
+/* Presents a directory listing to the page.
+ * pathlist is an array of strings, the names of the directories in the path.
+ * listing is a list of objects where each object is a file (containing
+ * several fields about the file).
+ */
+function presentlisting(pathlist, listing)
+{
+    presentpath(pathlist);
+    files = document.getElementById("files");
+
+    /* Create all of the files */
+    for each (file in listing)
+    {
+        /* Make a 'tr' element */
+        row = document.createElement("tr");
+        if (file.isdir)
+        {
+            row.appendChild(maketd("dir"));
+            navlink = JSON.stringify(navlist.concat([file.filename]));
+            navlink = "nav(" + navlink + ")";
+            row.appendChild(maketdwithlink(file.filename, "#", navlink));
+            row.appendChild(maketd(file.size));
+            row.appendChild(maketd(file.mtime));
+            row.appendChild(maketd(""));
+        }
+        else
+        {
+            row.appendChild(maketd("file"));
+            row.appendChild(maketd(file.filename));
+            row.appendChild(maketd(file.size));
+            row.appendChild(maketd(file.mtime));
+            row.appendChild(maketdwithactions(file.filename));
+        }
+        files.appendChild(row);
+    }
+
+}
+
+/* AJAX */
+
+function obj_to_query_string(pagename, obj)
+{
+    var hadone = false;
+    qs = pagename;
+    for (key in obj)
+    {
+        if (hadone == false)
+        {
+            qs += "?";
+            hadone = true;
+        }
+        else
+            qs += "&";
+        qs += escape(key) + "=" + escape(obj[key]);
+    }
+    return qs;
+}
+
+/* ACTIONS */
+
+function nav(pathlist)
+{
+    gpathlist = pathlist;
+    refresh();
+}
+
+function refresh()
+{ 
+    path = pathlisttopath(gpathlist);
+    url = obj_to_query_string("files.py/ls", {"path" : path});
+    var xmlhttp =  new XMLHttpRequest();
+        // false -> SYNCHRONOUS (wait for response)
+        // (No need for a callback function)
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send("");
+    listing = JSON.parse(xmlhttp.responseText);
+    if (listing == null)
+        alert("An error occured");
+    else if ("error" in listing)
+        alert("Error: " + listing.error);
+    else
+    {
+        clearfiles();
+        presentlisting(gpathlist, listing);
+    }
+}
+
+function rm(filename)
+{
+    path = pathlisttopath(gpathlist);
+    url = obj_to_query_string("files.py/rm",
+        {"path" : path, "filename" : filename});
+    var xmlhttp =  new XMLHttpRequest();
+        // false -> SYNCHRONOUS (wait for response)
+        // (No need for a callback function)
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send("");
+    listing = JSON.parse(xmlhttp.responseText);
+    if (listing == null)
+        alert("An error occured");
+    else if ("error" in listing)
+        alert("Error: " + listing.error);
+    else
+    {
+        clearfiles();
+        presentlisting(gpathlist, listing);
+    }
+}
+
+function rename(fromfilename)
+{
+    tofilename = prompt("Rename file \"" + fromfilename + "\" to?");
+    path = pathlisttopath(gpathlist);
+    url = obj_to_query_string("files.py/rename",
+        {"path" : path, "fromfilename" : fromfilename,
+        "tofilename" : tofilename});
+    var xmlhttp =  new XMLHttpRequest();
+        // false -> SYNCHRONOUS (wait for response)
+        // (No need for a callback function)
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send("");
+    listing = JSON.parse(xmlhttp.responseText);
+    if (listing == null)
+        alert("An error occured");
+    else if ("error" in listing)
+        alert("Error: " + listing.error);
+    else
+    {
+        clearfiles();
+        presentlisting(gpathlist, listing);
+    }
+}
+
+function newfile()
+{
+    window.location = "edit.html";
+}
+
+function edit(filename)
+{
+    window.location = "edit.html?filename="
+        + pathlisttopath(gpathlist) + filename;
+}
+
+function savefile()
+{
+    filename = document.getElementById("filename").value;
+    data = document.getElementById("data").value;
+    url = obj_to_query_string("files.py/putfile",
+        {"path" : "", "filename" : filename, "data" : data});
+    var xmlhttp =  new XMLHttpRequest();
+        // false -> SYNCHRONOUS (wait for response)
+        // (No need for a callback function)
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send("");
+    listing = JSON.parse(xmlhttp.responseText);
+    if (listing == null)
+        alert("An error occured");
+    else if ("error" in listing)
+        alert("Error: " + listing.error);
+}
+
+function loadfile()
+{
+    filename = document.getElementById("filename").value;
+    if (filename == null || filename == "")
+        return;
+    url = obj_to_query_string("files.py/getfile",
+        {"path" : "", "filename" : filename});
+    var xmlhttp =  new XMLHttpRequest();
+        // false -> SYNCHRONOUS (wait for response)
+        // (No need for a callback function)
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send("");
+    document.getElementById("data").value = xmlhttp.responseText;
+}
+
+/* Called on page load */
+function init_browser()
+{
+    gpathlist = [];
+    refresh();
+}
+
+function init_edit()
+{
+    document.getElementById("filename").value = getURLParam("filename");
+    loadfile();
+}
+
+/* BORROWED CODE */
+
+/* http://www.11tmr.com/11tmr.nsf/d6plinks/MWHE-695L9Z */
+function getURLParam(strParamName){
+  var strReturn = "";
+  var strHref = window.location.href;
+  if ( strHref.indexOf("?") > -1 ){
+    var strQueryString = strHref.substr(strHref.indexOf("?"));
+    var aQueryString = strQueryString.split("&");
+    for ( var iParam = 0; iParam < aQueryString.length; iParam++ ){
+      if (
+aQueryString[iParam].indexOf(strParamName.toLowerCase() + "=") > -1 ){
+        var aParam = aQueryString[iParam].split("=");
+        strReturn = aParam[1];
+        break;
+      }
+    }
+  }
+  return unescape(strReturn);
+}
---
+++ prototypes/browser/files.py
@@ -0,0 +1,180 @@
+# files.py - Publisher mod_python program
+#
+# Performs directory operations and returns result data in JSON format
+# Designed to be used with an AJAX front end.
+# PROOF-OF-CONCEPT ONLY
+#
+# Author: Matt Giuca
+
+# Note: All operations unless noted return a directory listing in JSON format.
+
+from mod_python import apache
+import os
+import time
+# JSON encoder
+import encoder
+
+content_type = "text/html"
+# application/json is the "best" content type but is not good for debugging
+# because Firefox just tries to download it
+#content_type = "application/json"
+enc = encoder.JSONEncoder()
+
+getfile_content_type="text/html"
+#getfile_content_type="application/octet-stream"
+
+root = "/home/mgiuca/www/htdocs/browser/playground/"
+
+greqprint = None
+def set_greqprint(req):
+    """Sets greqprint to a function which prints to the given req."""
+    global greqprint
+    def p(str):
+        req.write(str + '\n')
+    greqprint = p
+
+def safe_path_to_local(path):
+    """This function takes a path which is relative to the browser root
+    (global "root"). It normalises it and returns an absolute path on the
+    system. It checks to make sure the path is a SUBDIRECTORY of root,
+    and does not allow paths to go to a parent or sibling of root.
+    
+    Returns None if the path was invalid.
+    """
+    global root
+    # First normalise the path
+    path = os.path.normpath(path)
+    # Now if it begins with ".." then it's illegal
+    if path.startswith(".."):
+        return None
+    # Now join it onto the root
+    path = os.path.join(root, path)
+    # Just to be safe, normalise it again and make sure it is a subdirectory
+    # of root.
+    # Normcase only does anything on Windows
+    path = os.path.normcase(os.path.normpath(path))
+    if not path.startswith(os.path.normcase(os.path.normpath(root))):
+        return None
+    return path
+
+def init(req, path):
+    """Performs setup common to all actions (NOT a user-callable action).
+    Returns a new path which is safe (guaranteed within the root), and is an
+    absolute path."""
+    set_greqprint(req)
+    req.content_type = content_type
+    # filenames :: [string]
+    path = safe_path_to_local(path)
+    if path == None:
+        return "null"
+    return path
+
+def dirlisting(path):
+    """Get a directory listing from a path. The path is a safe, absolute path.
+    Returns a dictionary of file listing info. NOT to be called directly."""
+    filenames = os.listdir(path)
+    filenames.sort()
+    return map(file_to_fileinfo(path), filenames)
+
+def ls(req, path="."):
+    """Gets a directory listing of a given path on the server, in JSON
+    format."""
+    path = init(req, path)
+    try:
+        files = dirlisting(path)
+    except OSError, (errno, errmsg):
+        return enc.encode({"error": errmsg})
+    req.write(enc.encode(files))
+    return ""
+
+def rm(req, path, filename):
+    """Removes a file, and returns a directory listing of the path."""
+    # dirpath is the directory without the file attached
+    dirpath = init(req, path)
+    # filepath is the path to the file being deleted
+    filepath = safe_path_to_local(os.path.join(path, filename))
+    if filepath == None:
+        return "null"
+    try:
+        os.remove(filepath)
+        files = dirlisting(dirpath)
+    except OSError, (errno, errmsg):
+        return enc.encode({"error": errmsg})
+    req.write(enc.encode(files))
+    return ""
+
+def rename(req, path, fromfilename, tofilename):
+    """Renames a file, and returns a directory listing of the path."""
+    # dirpath is the directory without the file attached
+    dirpath = init(req, path)
+    # filepath is the path to the file being deleted
+    fromfilepath = safe_path_to_local(os.path.join(path, fromfilename))
+    tofilepath = safe_path_to_local(os.path.join(path, tofilename))
+    if fromfilepath == None or tofilepath == None:
+        return "null"
+    try:
+        os.rename(fromfilepath, tofilepath)
+        files = dirlisting(dirpath)
+    except OSError, (errno, errmsg):
+        return enc.encode({"error": errmsg})
+    req.write(enc.encode(files))
+    return ""
+
+def file_to_fileinfo(path):
+    """Given a filename (relative to a given path), gets all the info "ls"
+    needs to display about the filename. Returns a dict mapping a number of
+    fields which are returned."""
+    # Note: curried so it can be used with map
+    def ftf(filename):
+        fullpath = os.path.join(path, filename)
+        d = {"filename" : filename}
+        d["isdir"] = os.path.isdir(fullpath)
+        stat = os.stat(fullpath)
+        d["size"] = stat.st_size
+        d["mtime"] = time.ctime(stat.st_mtime)
+        return d
+    return ftf
+
+def getfile(req, path, filename):
+    """Returns the contents of a given file."""
+    filepath = init(req, os.path.join(path, filename))
+    req.content_type = getfile_content_type
+    # open the file and copy its contents into req
+    try:
+        file = open(filepath, "r")
+    except IOError, (errno, errmsg):
+        return enc.encode({"error": errmsg})
+
+    # TODO: Binary instead of per-line
+    for line in file:
+        req.write(line)
+    file.close()
+    return ""
+
+def putfile(req, path, filename, data):
+    """Writes data to a file, overwriting it if it exists and creating it if
+    it doesn't. The data should be postdata but this is a proof-of-concept.
+    Returns a directory listing, as the other commands do.
+    """
+    # dirpath is the directory without the file attached
+    dirpath = init(req, path)
+    # filepath is the path to the file being deleted
+    filepath = safe_path_to_local(os.path.join(path, filename))
+    if filepath == None:
+        return "null"
+    # open the file and copy its contents into req
+    try:
+        file = open(filepath, "w")
+    except IOError, (errno, errmsg):
+        return enc.encode({"error": errmsg})
+
+    file.write(data)
+    file.close()
+
+    try:
+        files = dirlisting(dirpath)
+    except OSError, (errno, errmsg):
+        return enc.encode({"error": errmsg})
+    req.write(enc.encode(files))
+    return ""
+
