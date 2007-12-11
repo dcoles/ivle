a205e040125bbd38cd8a6d1b3455b0f4bf5a80ae
mattgiuca <>
2007-12-11 12:37:06 +0000

    Added new prototype: browser-interface. (This is independent of browser. It is an HTML/CSS prototype, does not have any server backend, just a visual demo).

---
+++ prototypes/browser/browser-interface/browser.js
@@ -0,0 +1,8 @@
+/* browser.js
+ * File Browser Interface Prototype
+ * (Unlike the first prototype this does not interact with a server, it is
+ * just a graphical interface demo)
+ *
+ * Author: Matt Giuca
+ */
+
