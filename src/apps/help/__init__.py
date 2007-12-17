c770fd34c124f4bb0056e27260fe1cd3a4661c9c
mattgiuca <>
2007-12-17 05:27:38 +0000

    apps/help: Now displays a top-level menu of all apps with help documents.

--- src/apps/help/__init__.py
+++ src/apps/help/__init__.py
@@ -32,15 +32,41 @@ from conf import apps
 def handle(req):
     """Handler for the Help application."""
     (appurl, subpath) = util.split_path(req.path)
-    # app must be valid and have help available
-    if appurl not in conf.apps.app_url:
-        req.throw_error(req.HTTP_NOT_FOUND)
-    app = conf.apps.app_url[appurl]
-    if not app.hashelp:
-        req.throw_error(req.HTTP_NOT_FOUND)
-    # subpath must be empty, for now, as there is only one help file per app
-    if subpath != "":
-        req.throw_error(req.HTTP_NOT_FOUND)
+
+    if appurl is None:
+        show_help_menu(req)
+    else:
+        # app must be valid and have help available
+        if appurl not in conf.apps.app_url:
+            req.throw_error(req.HTTP_NOT_FOUND)
+        app = conf.apps.app_url[appurl]
+        if not app.hashelp:
+            req.throw_error(req.HTTP_NOT_FOUND)
+        # subpath must be empty, for now, as there is only one help file per app
+        if subpath != "":
+            req.throw_error(req.HTTP_NOT_FOUND)
+        show_help_app(req, app)
+
+def show_help_menu(req):
+    """Show the help menu."""
+
+    # Set request attributes
+    req.content_type = "text/html"
+    req.write_html_head_foot = True
+
+    # Start writing data
+    req.write("<h1>Help</h1>\n")
+
+    # Write a list of links to all apps with help modules
+    req.write("<ul>\n")
+    for (appurl, app) in conf.apps.app_url.items():
+        if app.hashelp:
+            req.write('  <li><a href="%s">%s</a></li>\n'
+                % (os.path.join(util.make_path("help"), appurl), app.name))
+    req.write("</ul>\n")
+
+def show_help_app(req, app):
+    """Show help for an application."""
     helpfile = os.path.join(util.make_local_path("apps"), app.dir, "help.html")
 
     # Set request attributes
