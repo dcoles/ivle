8ce00f592bc609674708f0b53bf4327f22ab82bf
drtomc <>
2008-01-02 04:45:20 +0000

    A basic version of the console going!

--- console/console.js
+++ console/console.js
@@ -1,19 +1,90 @@
-var magic;
+var magic = 'xyzzy';
+
+function historyUp()
+{
+    if (this.cursor >= 0)
+    {
+        this.cursor--;
+    }
+}
+
+function historyDown()
+{
+    if (this.cursor < this.items.length)
+    {
+        this.cursor++;
+    }
+}
+
+function historyCurr()
+{
+    if (this.cursor < 0 || this.cursor >= this.items.length)
+    {
+        return "";
+    }
+    return this.items[this.cursor];
+}
+
+function historyAdd(text)
+{
+    this.items[this.items.length] = text;
+    this.cursor = this.items.length
+}
+
+function historyShow()
+{
+    var res = "";
+    if (this.cursor == -1)
+    {
+        res += "[]";
+    }
+    for (var i = 0; i < this.items.length; i++)
+    {
+        if (i == this.cursor)
+        {
+            res += "["
+        }
+        res += this.items[i].toString();
+        if (i == this.cursor)
+        {
+            res += "]"
+        }
+        res += " "
+    }
+    if (this.cursor == this.items.length)
+    {
+        res += "[]";
+    }
+    return res;
+}
+
+function History()
+{
+    this.items = new Array();
+    this.cursor = -1;
+    this.up = historyUp;
+    this.down = historyDown;
+    this.curr = historyCurr;
+    this.add = historyAdd;
+    this.show = historyShow;
+}
+
+var hist = new History();
 
 function make_query_string(pagename, args)
 {
     var first = true;
     var qs = pagename;
-    for (key in obj)
+    for (key in args)
     {
-        vals = obj[key];
+        vals = args[key];
         // vals can be an array, to make multiple args with the same name
         // To handle this, make non-array objects into an array, then loop
         if (!(vals instanceof Array))
             vals = [vals];
         for each (val in vals)
         {
-            if (true)
+            if (first)
             {
                 qs += "?";
                 first = false;
@@ -28,28 +99,87 @@ function make_query_string(pagename, args)
     return qs;
 }
 
-function enter_line(inp)
+function make_post_body(args)
 {
-    var digest = hex_md5(inp + magic);
-    var url = make_query_string("chat", {"digest":digest, "text":inp});
-    var xmlhttp = XMLHttpRequest();
-    xmlhttp.open("POST", url, false);
-    xmlhttp.send("")
-    var res = JSON.parse(xmlhttp..responseText);
+    var first = true;
+    var qs = '';
+    for (key in args)
+    {
+        vals = args[key];
+        // vals can be an array, to make multiple args with the same name
+        // To handle this, make non-array objects into an array, then loop
+        if (!(vals instanceof Array))
+            vals = [vals];
+        for each (val in vals)
+        {
+            if (first)
+            {
+                first = false;
+            }
+            else
+            {
+                qs += "&";
+            }
+            qs += encodeURI(key) + "=" + encodeURI(val);
+        }
+    }
+    return qs;
+}
+
+function enter_line()
+{
+    var inp = document.getElementById('inputText');
+    var digest = hex_md5(inp.value + magic);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", "chat", false);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(make_post_body({"digest":digest, "text":inp.value}))
+    var res = JSON.parse(xmlhttp.responseText);
+    var output = document.getElementById("outputArea")
+    output.appendChild(document.createTextNode(inp.value + "\n"));
     if (res && res[0])
     {
         // Success!
         // print out the output (res[0])
+        output.appendChild(document.createTextNode(res[0]));
         // print out the return value (res[1])
+        if (res[1])
+        {
+            output.appendChild(document.createTextNode(res[1] + "\n"));
+        }
         // set the prompt to >>>
+        var prompt = document.getElementById("prompt");
+        prompt.replaceChild(document.createTextNode(">>> "), prompt.firstChild);
     }
     else if (res)
     {
         // Failure!
         // print out the error message (res[2])
+        output.appendChild(document.createTextNode(res[2]));
     }
     else
     {
         // Need more input, so set the prompt to ...
+        var prompt = document.getElementById("prompt");
+        prompt.replaceChild(document.createTextNode("... "), prompt.firstChild);
     }
+}
+
+function catch_input(key)
+{
+    var inp = document.getElementById('inputText');
+    if (key == 13)
+    {
+        enter_line();
+        hist.add(inp.value);
+    }
+    if (key == 38)
+    {
+        hist.up();
+    }
+    if (key == 40)
+    {
+        hist.down();
+    }
+    inp.value = hist.curr();
 }
