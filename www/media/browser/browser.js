8fd7c9ed9acdcb7f35aab6d95704847a4ed85178
mattgiuca <>
2008-01-11 05:36:45 +0000

    browser.js: Added functions do_action, navigate, handle_response (stubs). Top-level functions for interacting with the server.

--- www/media/browser/browser.js
+++ www/media/browser/browser.js
@@ -20,6 +20,68 @@
  * Date: 11/1/2008
  */
 
+/** Calls the server using Ajax, performing an action on the server side.
+ * Receives the response from the server and performs a refresh of the page
+ * contents, updating it to display the returned data (such as a directory
+ * listing, file preview, or editor pane).
+ * Always makes a POST request.
+ * No return value.
+ *
+ * \param action String. Name of the action to perform, as defined in the
+ *     fileservice API.
+ * \param path URL path to make the request to, within the application.
+ * \param args Argument object, as described in util.parse_url and friends.
+ *      This should contain the arguments to the action, but NOT the action
+ *      itself. (Also a minor side-effect; the "args" object will be mutated
+ *      to include the action attribute).
+ * \param content_type String, optional.
+ *      May be "application/x-www-form-urlencoded" or "multipart/form-data".
+ *      Defaults to "application/x-www-form-urlencoded".
+ *      "multipart/form-data" is recommended for large uploads.
+ */
+function do_action(action, path, args, content_type)
+{
+    args.action = action;
+    /* Call the server and perform the action. This mutates the server. */
+    response = call_ajax("fileservice", path, args, "POST", content_type);
+    /* Check for action errors reported by the server, and report them to the
+     * user */
+    /* TODO */
+    /* Now read the response and set up the page accordingly */
+    handle_response(path, response);
+}
+
+/** Calls the server using Ajax, requesting a directory listing. This should
+ * not modify the server in any way. Receives the response from the server and
+ * performs a refresh of the page contents, updating it to display the
+ * returned data (such as a directory listing, file preview, or editor pane).
+ * Called "navigate", can also be used for a simple refresh.
+ * Always makes a GET request.
+ * No return value.
+ */
+function navigate(path)
+{
+    /* Call the server and request the listing. This mutates the server. */
+    response = call_ajax("fileservice", path, null, "GET");
+    /* Now read the response and set up the page accordingly */
+    handle_response(path, response);
+}
+
+/** Given an HTTP response object, cleans up and rebuilds the contents of the
+ * page using the response data. This does not navigate away from the page, it
+ * merely rebuilds most of the data.
+ * Note that depending on the type of data returned, this could result in a
+ * directory listing, an image preview, an editor pane, etc.
+ * Figures out the type and calls the appropriate function.
+ * \param path URL path which the request was made for. This can (among other
+ * things) be used to update the URL in the location bar.
+ * \param response XMLHttpRequest object returned by the server. Should
+ * contain all the response data.
+ */
+function handle_response(path, response)
+{
+}
+
 /** Called when the page loads initially.
  */
 window.onload = function()
