d8bb89c2d813e7a7ed812b627cfa930348e23b52
mattgiuca <>
2008-02-01 05:51:53 +0000

    Added new module: common/db.py. This is a Data Access Layer which will handle ALL direct DB interaction. No other module should import pg, or feature any SQL code strings.
    Currently implements the DB class, and create_user and drop_user
    functionality.

---
+++ www/common/db.py
@@ -0,0 +1,87 @@
+# IVLE - Informatics Virtual Learning Environment
+# Copyright (C) 2007-2008 The University of Melbourne
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# Module: Database
+# Author: Matt Giuca
+# Date:   1/2/2008
+
+# Code to talk to the PostgreSQL database.
+# (This is the Data Access Layer).
+# All DB code should be in this module to ensure portability if we want to
+# change the DB implementation.
+# This means no SQL strings should be outside of this module. Add functions
+# here to perform the activities needed, and place the SQL code for those
+# activities within.
+
+# CAUTION to editors of this module.
+# All string inputs must be sanitized by calling _escape before being
+# formatted into an SQL query string.
+
+import pg
+import conf
+import md5
+
+def _escape(str):
+    """Wrapper around pg.escape_string. Escapes the string for use in SQL, and
+    also quotes it to make sure that every string used in a query is quoted.
+    """
+    return "'" + pg.escape_string(str) + "'"
+
+class DB:
+    """An IVLE database object. This object provides an interface to
+    interacting with the IVLE database without using any external SQL.
+
+    Most methods of this class have an optional dry argument. If true, they
+    will return the SQL query string and NOT actually execute it. (For
+    debugging purposes).
+    """
+    def __init__(self):
+        """Connects to the database and creates a DB object.
+        Takes no parameters - gets all the DB info from the configuration."""
+        self.db = pg.connect(dbname=conf.db_dbname, host=conf.db_host,
+                port=conf.db_port, user=conf.db_user, passwd=conf.db_password)
+
+    @staticmethod
+    def _passhash(password):
+        return md5.md5(password).hexdigest()
+
+    def create_user(self, login, password, nick, fullname, rolenm, studentid,
+        dry=False):
+        """Creates a user login entry in the database.
+        Arguments are the same as those in the "login" table of the schema.
+        The exception is "password", which is a cleartext password. makeuser
+        will hash the password.
+        """
+        passhash = DB._passhash(password)
+        query = ("INSERT INTO login (login, passhash, nick, fullname, "
+            "rolenm, studentid) VALUES (%s, %s, %s, %s, %s, %s);" %
+            (_escape(login), _escape(passhash), _escape(nick),
+            _escape(fullname), _escape(rolenm), _escape(studentid)))
+        if dry: return query
+        self.db.query(query)
+
+    def drop_user(self, login, dry=False):
+        """Deletes a user login entry from the database."""
+        query = "DELETE FROM login WHERE login = %s;" % _escape(login)
+        if dry: return query
+        self.db.query(query)
+
+    def close(self):
+        """Close the DB connection. Do not call any other functions after
+        this. (The behaviour of doing so is undefined).
+        """
+        self.db.close()
