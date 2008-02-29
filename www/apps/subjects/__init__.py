1e8acee1af1da4a43b52bf8de707472738188be7
mattgiuca <>
2008-02-29 02:28:04 +0000

    subjects: Now presents a top-level subjects menu (same as tutorials).     And is able to present any other path with an iframe to that path within     subjects.     (Box is too small at the moment, style later).

--- www/apps/subjects/__init__.py
+++ www/apps/subjects/__init__.py
@@ -22,12 +22,52 @@
 # This is an IVLE application.
 # A sample / testing application for IVLE.
 
+import os
+import urllib
+import cgi
+
 from common import util
 
 def handle(req):
     """Handler for the Subjects application. Links to subject home pages."""
 
+    if req.path == "":
+        handle_toplevel_menu(req)
+    else:
+        handle_subject_page(req, req.path)
+
+def handle_toplevel_menu(req):
+    # This is represented as a directory. Redirect and add a slash if it is
+    # missing.
+    if req.uri[-1] != '/':
+        req.throw_redirect(make_tutorial_path())
+
+    # Get list of subjects
+    # TODO: Fetch from DB. For now, just get directory listing
+    try:
+        subjects = os.listdir(util.make_local_path(os.path.join('media',
+            'subjects')))
+    except OSError:
+        req.throw_error(req.HTTP_INTERNAL_SERVER_ERROR,
+            "There are is no subject homepages directory.")
+    subjects.sort()
+
+    req.content_type = "text/html"
+    req.write_html_head_foot = True
+    req.write('<div id="ivle_padding">\n')
+    req.write("<h2>IVLE Subject Homepages</h2>\n")
+    req.write("<h2>Subjects</h2>\n<ul>\n")
+    for subject in subjects:
+        req.write('  <li><a href="%s">%s</a></li>\n'
+            % (urllib.quote(subject) + '/', cgi.escape(subject)))
+    req.write("</ul>\n")
+    req.write("</div>\n")
+
+def handle_subject_page(req, path):
     req.content_type = "text/html"
     req.write_html_head_foot = True     # Have dispatch print head and foot
 
-    req.write("<p>TODO: Subject homepages</p>\n")
+    # Just make the iframe pointing to media/subjects
+    serve_loc = util.make_path(os.path.join('media', 'subjects', path))
+    req.write('<iframe src="%s"></iframe>'
+        % urllib.quote(serve_loc))
