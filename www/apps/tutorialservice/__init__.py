ece99fc689c3ae51bf442ee9aa843d95cea3e930
mattgiuca <>
2008-08-18 10:05:46 +0000

    tutorialservice: Added two new GET actions: getattempts and getattempt.     getattempts retrieves a list of all attempts the user has made for a     particular question.     getattempt returns the code for a specific attempt.     Note that BY DEFAULT, these do not allow you to browse disabled attempts     (but this can be easily activated via a flag at the top of     tutorialservice/__init__.py). db.py: Added get_problem_attempts and get_problem_attempt methods to do the DB     queries for the above actions.

--- www/apps/tutorialservice/__init__.py
+++ www/apps/tutorialservice/__init__.py
@@ -32,6 +32,12 @@
 #              relative to the subjects base directory.
 # action "save" or "test" (POST only):
 #   "code" - Full text of the student's code being submitted.
+# action "getattempts": No arguments. Returns a list of
+#   {'date': 'formatted_date', 'complete': bool} dicts.
+# action "getattempt":
+#   "date" - Formatted date. Gets most recent attempt before (and including)
+#   that date.
+#   Returns JSON string containing code, or null.
 
 # Returns a JSON response string indicating the results.
 
@@ -43,6 +49,10 @@ import cjson
 from common import (db, util)
 import test
 import conf
+
+# If True, getattempts or getattempt will allow browsing of inactive/disabled
+# attempts. If False, will not allow this.
+HISTORY_ALLOW_INACTIVE = False
 
 def handle(req):
     """Handler for Ajax backend TutorialService app."""
@@ -63,8 +73,8 @@ def handle(req):
         # Must be POST
         if req.method != 'POST':
             req.throw_error(req.HTTP_BAD_REQUEST)
-        code = fields.getfirst('code')
 
+        code = fields.getfirst('code')
         if code is None:
             req.throw_error(req.HTTP_BAD_REQUEST)
         code = code.value
@@ -73,6 +83,22 @@ def handle(req):
             handle_save(req, exercise, code, fields)
         else:   # act == "test"
             handle_test(req, exercise, code, fields)
+    elif act == 'getattempts':
+        handle_getattempts(req, exercise)
+    elif act == 'getattempt':
+        date = fields.getfirst('date')
+        if date is None:
+            req.throw_error(req.HTTP_BAD_REQUEST)
+        date = date.value
+        # Convert into a struct_time
+        # The time *should* be in the same format as the DB (since it should
+        # be bounced back to us from the getattempts output). Assume this.
+        try:
+            date = time.strptime(date, db.TIMESTAMP_FORMAT)
+        except ValueError:
+            # Date was not in correct format
+            req.throw_error(req.HTTP_BAD_REQUEST)
+        handle_getattempt(req, exercise, date)
     else:
         req.throw_error(req.HTTP_BAD_REQUEST)
 
@@ -133,5 +159,31 @@ def handle_test(req, exercise, code, fields):
         test_results["attempts"] = attempts
 
         req.write(cjson.encode(test_results))
+    finally:
+        conn.close()
+
+def handle_getattempts(req, exercise):
+    """Handles a getattempts action."""
+    conn = db.DB()
+    try:
+        attempts = conn.get_problem_attempts(
+            login=req.user.login,
+            exercisename=exercise,
+            allow_inactive=HISTORY_ALLOW_INACTIVE)
+        req.write(cjson.encode(attempts))
+    finally:
+        conn.close()
+
+def handle_getattempt(req, exercise, date):
+    """Handles a getattempts action. Date is a struct_time."""
+    conn = db.DB()
+    try:
+        attempt = conn.get_problem_attempt(
+            login=req.user.login,
+            exercisename=exercise,
+            as_of=date,
+            allow_inactive=HISTORY_ALLOW_INACTIVE)
+        # attempt may be None; will write "null"
+        req.write(cjson.encode(attempt))
     finally:
         conn.close()
