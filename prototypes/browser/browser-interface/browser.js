b0d3fef6705573b0da848beefb5e06ec931e5bd2
matt.giuca <>
2009-01-12 00:36:24 +0000

    Removed "prototypes" directory (very old and clearly no longer used).

--- prototypes/browser/browser-interface/browser.js
+++
@@ -1,8 +0,0 @@
-/* browser.js
- * File Browser Interface Prototype
- * (Unlike the first prototype this does not interact with a server, it is
- * just a graphical interface demo)
- *
- * Author: Matt Giuca
- */
-
