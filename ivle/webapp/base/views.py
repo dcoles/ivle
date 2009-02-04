0a31c61ff1a66de9356dd10c00cedcf3097b7949
Nick Chadwick chadnickbok@gmail.com <>
2009-02-04 14:07:01 +1100

    Created a new XHTMLView, which uses a Genshi template for rendering the app.
    Also added ivle-headings.html, which is the new template for
    the site

--- ivle/webapp/base/views.py
+++ ivle/webapp/base/views.py
@@ -17,7 +17,14 @@
 
 # Author: Matt Giuca, Will Grant
 
+import inspect
+import os.path
+
 import cjson
+import genshi.template
+
+import ivle.conf
+import ivle.util
 
 from ivle.webapp.errors import BadRequest
 
@@ -70,3 +77,62 @@ class JSONRESTView(RESTView):
         if outjson is not None:
             req.write(cjson.encode(outjson))
             req.write("\n")
+            
+class XHTMLView(BaseView):
+    """
+    A view which provides a base class for views which need to return XHTML
+    It is expected that apps which use this view will be written using Genshi
+    templates.
+    """
+    def __init__(self, req, **kwargs):
+        for key in kwargs:
+          setattr(self, key, kwargs[key])
+        
+    def render(self, req):
+        ctx = genshi.template.Context()
+        self.populate(req, ctx)
+        self.populate_headings(req, ctx)
+        
+        ctx['app_styles'] = req.styles
+        ctx['scripts'] = req.scripts
+        ctx['scripts_init'] = req.scripts_init
+        app_template = os.path.join(os.path.dirname(
+                        inspect.getmodule(self).__file__), self.app_template) 
+        req.write_html_head_foot = False
+        loader = genshi.template.TemplateLoader(".", auto_reload=True)
+        tmpl = loader.load(app_template)
+        app = tmpl.generate(ctx)
+        ctx['app_template'] = app
+        tmpl = loader.load(os.path.join(os.path.dirname(__file__), 
+                                                        'ivle-headings.html'))
+        req.write(tmpl.generate(ctx).render('xhtml', doctype='xhtml'))
+
+    def populate_headings(self, req, ctx):
+        ctx['favicon'] = None
+        ctx['root_dir'] = ivle.conf.root_dir
+        ctx['public_host'] = ivle.conf.public_host
+        if req.user:
+            ctx['login'] = req.user.login
+            ctx['logged_in'] = True
+            ctx['nick'] = req.user.nick
+        else:
+            ctx['login'] = None
+        ctx['publicmode'] = req.publicmode
+        ctx['apps_in_tabs'] = []
+        for urlname in ivle.conf.apps.apps_in_tabs:
+            new_app = {}
+            app = ivle.conf.apps.app_url[urlname]
+            new_app['this_app'] = urlname == self.appname
+            if app.icon:
+                new_app['has_icon'] = True
+                icon_dir = ivle.conf.apps.app_icon_dir
+                icon_url = ivle.util.make_path(os.path.join(icon_dir, app.icon))
+                new_app['icon_url'] = icon_url
+                if new_app['this_app']:
+                    ctx['favicon'] = icon_url
+            else:
+                new_app['has_icon'] = False
+            new_app['path'] = ivle.util.make_path(urlname)
+            new_app['desc'] = app.desc
+            new_app['name'] = app.name
+            ctx['apps_in_tabs'].append(new_app)
