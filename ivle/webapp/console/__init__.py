fe394952b9c06e211a9c3737a9f0a4320052b1db
William Grant <grantw@unimelb.edu.au>
2009-02-09 14:19:58 +1100

    Set appname on ivle.webapp.console.ConsoleView so we get an icon and selected tab.

--- ivle/webapp/console/__init__.py
+++ ivle/webapp/console/__init__.py
@@ -6,12 +6,15 @@ from ivle.webapp.base.xhtml import XHTMLView
 
 
 class ConsoleView(XHTMLView):
+    appname = 'console'
+
     plugin_scripts = {'ivle.webapp.console': ['console.js']}
     plugin_styles  = {'ivle.webapp.console': ['console.css']}
     plugin_scripts_init = ['console_init']
+
     # Don't load the console overlay when we already have a console.
     overlay_blacklist = [ConsoleOverlay]
-    
+
     def populate(self, req, ctx):
         ctx['windowpane'] = False
 
