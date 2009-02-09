74e77e1d8d1baccdaef703dcf7a9f0eb5424d9df
Nick Chadwick <chadnickbok@gmail.com>
2009-02-09 12:12:26 +1100

    Added a console plugin, which now uses our new architecture.

--- ivle/webapp/console/__init__.py
+++ ivle/webapp/console/__init__.py
@@ -2,6 +2,19 @@ from ivle.webapp.base.plugins import ViewPlugin
 from ivle.webapp.base.plugins import OverlayPlugin
 from ivle.webapp.console.service import ConsoleServiceRESTView
 from ivle.webapp.console.overlay import ConsoleOverlay
+from ivle.webapp.base.xhtml import XHTMLView
+
+
+class ConsoleView(XHTMLView):
+    plugin_scripts = {'ivle.webapp.console': ['console.js']}
+    plugin_styles  = {'ivle.webapp.console': ['console.css']}
+    plugin_scripts_init = ['console_init']
+    # Don't load the console overlay when we already have a console.
+    overlay_blacklist = [ConsoleOverlay]
+    
+    def populate(self, req, ctx):
+        ctx['windowpane'] = False
+
 
 class Plugin(ViewPlugin, OverlayPlugin):
     """
@@ -9,6 +22,7 @@ class Plugin(ViewPlugin, OverlayPlugin):
     """
     urls = [
         ('console/service', ConsoleServiceRESTView),
+        ('console', ConsoleView),
     ]
     overlays = [
         ConsoleOverlay,
