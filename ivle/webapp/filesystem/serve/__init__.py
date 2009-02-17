718842c7eec073a83773db86e1133eb170779f89
William Grant <grantw@unimelb.edu.au>
2009-02-17 18:27:34 +1100

    Provide a public serve view. Under /~user/path/to/script. How shiny.

--- ivle/webapp/filesystem/serve/__init__.py
+++ ivle/webapp/filesystem/serve/__init__.py
@@ -32,7 +32,7 @@ from ivle import (studpath, interpret)
 import ivle.conf
 from ivle.database import User
 from ivle.webapp.base.views import BaseView
-from ivle.webapp.base.plugins import ViewPlugin
+from ivle.webapp.base.plugins import ViewPlugin, PublicViewPlugin
 from ivle.webapp.errors import NotFound, Unauthorized, Forbidden
 
 class ServeView(BaseView):
@@ -56,7 +56,76 @@ class ServeView(BaseView):
         self.serve(req, owner, jail, path)
 
     def serve(self, req, owner, jail, path):
-        serve_file(req, owner, jail, path)
+        self.serve_file(req, owner, jail, path)
+
+    def authorize(self, req):
+        """Given a request, checks whether req.username is allowed to
+        access req.path. Returns True on authz success, False on failure.
+        """
+        # Private mode authorization: standard (only logged in user can access
+        # their own files, and can access all of them).
+        return studpath.authorize(req, req.user)
+
+    def serve_file(self, req, owner, jail, path, download=False, files=None):
+        """Serves a file, using one of three possibilities: interpreting it,
+        serving it directly, or denying it and returning a 403 Forbidden error.
+        No return value. Writes to req (possibly throwing an HTTP error).
+
+        req: An IVLE request object.
+        owner: The user who owns the file being served.
+        jail: The user's jail.
+        path: Filename in the jail.
+        download:  Should the file be viewed in browser or downloaded
+        """
+
+        # We need a no-op trampoline run to ensure that the jail is mounted.
+        # Otherwise we won't be able to authorise for public mode!
+        noop_object = interpret.interpreter_objects["noop"]
+        interpret.interpret_file(req, owner, jail, '', noop_object)
+
+        # Authorize access. If failure, this throws a HTTP_FORBIDDEN error.
+        if not self.authorize(req):
+            raise Unauthorized()
+
+        args = []
+        if download:
+            args.append('-d')
+
+        if files and download:
+            args += [os.path.join(path, f) for f in files]
+        else:
+            args.append(path)
+
+        (out, err) = ivle.interpret.execute_raw(owner, jail, '/home',
+                    os.path.join(ivle.conf.share_path, 'services/serveservice'),
+                    args)
+        assert not err
+
+        # Remove the JSON from the front of the response, and decode it.
+        json = out.split('\n', 1)[0]
+        out = out[len(json) + 1:]
+        response = cjson.decode(json)
+
+        if 'error' in response:
+            if response['error'] == 'not-found':
+                raise NotFound()
+            elif response['error'] in ('is-directory', 'forbidden'):
+                raise Forbidden()
+            elif response['error'] == 'is-executable':
+                # We need to execute it. Just run it with Python in the jail.
+                interp_object = interpret.interpreter_objects["cgi-python"]
+                interpret.interpret_file(req, owner, jail, response['path'],
+                                         interp_object, gentle=True)
+                return
+            else:
+                raise AssertionError('Unknown error from serveservice: %s' %
+                                     response['error'])
+
+        if download:
+            req.headers_out["Content-Disposition"] = \
+                         "attachment; filename=%s" % response['name']
+        req.content_type = response['type']
+        req.write(out)
 
 class DownloadView(ServeView):
     def __init__(self, req, path):
@@ -68,87 +137,25 @@ class DownloadView(ServeView):
             self.files = None
 
     def serve(self, req, owner, jail, path):
-        serve_file(req, owner, jail, path, download=True, files=self.files)
+        self.serve_file(req, owner, jail, path, download=True,files=self.files)
 
-def authorize(req):
-    """Given a request, checks whether req.username is allowed to
-    access req.path. Returns True on authorization success, False on failure.
-    """
-    # TODO: Reactivate public mode.
-    #if req.publicmode:
+class PublicServeView(ServeView):
+    def __init__(self, req, path):
+        req.path = path
+        super(PublicServeView, self).__init__(req, path)
+
+    def authorize(self, req):
         # Public mode authorization: any user can access any other user's
-        # files, BUT the accessed file needs to have its "ivle:published" flag
-        # turned on in the SVN status.
-    #    studpath.authorize_public(req)
+        # files, BUT the accessed file needs to have a file named '.published'
+        # in its parent directory.
+        return studpath.authorize_public(req)
 
-    # Private mode authorization: standard (only logged in user can access
-    # their own files, and can access all of them).
-    return studpath.authorize(req, req.user)
-
-def serve_file(req, owner, jail, path, download=False, files=None):
-    """Serves a file, using one of three possibilities: interpreting the file,
-    serving it directly, or denying it and returning a 403 Forbidden error.
-    No return value. Writes to req (possibly throwing a server error exception
-    using req.throw_error).
-
-    req: An IVLE request object.
-    owner: The user who owns the file being served.
-    jail: The user's jail.
-    path: Filename in the jail.
-    download:  Should the file be viewed in browser or downloaded
-    """
-
-    # We need a no-op trampoline run to ensure that the jail is mounted.
-    # Otherwise we won't be able to authorise for public mode!
-    noop_object = interpret.interpreter_objects["noop"]
-    interpret.interpret_file(req, owner, jail, '', noop_object)
-
-    # Authorize access. If failure, this throws a HTTP_FORBIDDEN error.
-    if not authorize(req):
-        raise Unauthorized()
-
-    args = []
-    if download:
-        args.append('-d')
-
-    if files and download:
-        args += [os.path.join(path, f) for f in files]
-    else:
-        args.append(path)
-
-    (out, err) = ivle.interpret.execute_raw(req.user, jail, '/home',
-                os.path.join(ivle.conf.share_path, 'services/serveservice'),
-                args)
-    assert not err
-
-    # Remove the JSON from the front of the response, and decode it.
-    json = out.split('\n', 1)[0]
-    out = out[len(json) + 1:]
-    response = cjson.decode(json)
-
-    if 'error' in response:
-        if response['error'] == 'not-found':
-            raise NotFound()
-        elif response['error'] in ('is-directory', 'forbidden'):
-            raise Forbidden()
-        elif response['error'] == 'is-executable':
-            # We need to execute it. Just run it with Python in the jail.
-            interp_object = interpret.interpreter_objects["cgi-python"]
-            interpret.interpret_file(req, owner, jail, response['path'],
-                                     interp_object, gentle=True)
-            return
-        else:
-            raise AssertionError('Unknown error from serveservice: %s' %
-                                 response['error'])
-
-    if download:
-        req.headers_out["Content-Disposition"] = \
-                     "attachment; filename=%s" % response['name']
-    req.content_type = response['type']
-    req.write(out)
-
-class Plugin(ViewPlugin):
+class Plugin(ViewPlugin, PublicViewPlugin):
     urls = [
         ('serve/*path', ServeView),
         ('download/*path', DownloadView),
+    ]
+
+    public_urls = [
+        ('~*path', PublicServeView),
     ]
