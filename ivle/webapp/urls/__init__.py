043c23db4c4a611a605f1385447694576b60641a
William Grant <grantw@unimelb.edu.au>
2009-07-01 21:59:49 +1000

    Add an object-traversal-based router.

---
+++ ivle/webapp/urls/__init__.py
@@ -0,0 +1,192 @@
+# IVLE - Informatics Virtual Learning Environment
+# Copyright (C) 2007-2009 The University of Melbourne
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""Object traversal URL utilities."""
+
+import os.path
+import sys
+
+ROOT = object() # Marker object for the root.
+
+class RoutingError(Exception):
+    pass
+
+class NotFound(RoutingError):
+    """The path did not resolve to an object."""
+    pass
+
+class RoutelessObject(NotFound):
+    """The path led to an object with no routes."""
+    pass
+
+class InsufficientPathSegments(NotFound):
+    """The path led to a route that expected more arguments."""
+    pass
+
+class NoPath(RoutingError):
+    """There is no path from the given object to the root."""
+    pass
+
+class RouteConflict(RoutingError):
+    """A route with the same discriminator is already registered."""
+    pass
+
+def _segment_path(path):
+    """Split a path into its segments, after normalisation.
+
+       >>> _segment_path('/path/to/something')
+       ['path', 'to', 'something']
+       >>> _segment_path('/path/to/something/')
+       ['path', 'to', 'something']
+       >>> _segment_path('/')
+       []
+    """
+
+    segments = os.path.normpath(path).split('/')
+
+    # Remove empty segments caused by leading and trailing seperators.
+    if segments[0] == '':
+        segments.pop(0)
+    if segments[-1] == '':
+        segments.pop()
+    return segments
+
+class Router(object):
+    '''Router to resolve and generate paths.
+
+    Maintains a registry of forward and reverse routes, dealing with paths
+    to objects and views published in the URL space.
+    '''
+
+    def __init__(self, root, default='+index'):
+        self.fmap = {} # Forward map.
+        self.rmap = {} # Reverse map.
+        self.root = root
+        self.default = default
+
+    def add_forward(self, src, segment, func, argc):
+        """Register a forward (path resolution) route."""
+
+        if src not in self.fmap:
+            self.fmap[src] = {}
+
+        # If a route already exists with the same source and name, we have a
+        # conflict. We don't like conflicts.
+        if segment in self.fmap[src]:
+            raise RouteConflict((src, segment, func),
+                                (src, segment, self.fmap[src][segment][0]))
+
+        self.fmap[src][segment] = (func, argc)
+
+
+    def add_reverse(self, src, func):
+        """Register a reverse (path generation) route."""
+
+        if src in self.rmap:
+             raise RouteConflict((src, func), (src, self.rmap[src]))
+        self.rmap[src] = func
+
+    def resolve(self, path):
+        """Resolve a path into an object.
+
+        Traverses the tree of routes using the given path.
+        """
+
+        (obj, todo) = self._traverse(_segment_path(path), self.root)
+
+        # If we have no segments remaining, let's try the default route.
+        # If there is no default route for this object, we don't care.
+        if len(todo) == 0:
+            obj = self._traverse([self.default], obj)[0]
+
+        return obj
+
+    def generate(self, obj):
+        """Resolve an object into a path.
+
+        Traverse up the tree of reverse routes, generating a path which
+        resolves to the object.
+        """
+
+        # Attempt to get all the way to the top. Each reverse route should
+        # return a (parent, pathsegments) tuple.
+        curobj = obj
+        names = []
+
+        # None represents the root.
+        while curobj is not ROOT:
+            route = self.rmap.get(type(curobj))
+            if route is None:
+                raise NoPath(obj, curobj)
+            (curobj, newnames) = route(curobj)
+
+            # The reverse route can return either a string of one segment,
+            # or a tuple of many.
+            if isinstance(newnames, basestring):
+                names.insert(0, newnames)
+            else:
+                names = list(newnames) + list(names)
+
+        # Generate nice URLs for the default route, if it is the last. We need
+        # to have an empty path rather than none at all in order to preserve
+        # the trailing /.
+        if names[-1] == self.default:
+            del names[-1]
+
+        return os.path.join('/', *names)
+
+    def _traverse(self, todo, obj):
+        """Populate the object stack given a list of path segments.
+
+        Traverses the forward route tree, using the given path segments.
+
+        Intended to be used by route(), and nobody else.
+        """
+        while todo:
+            # Check if we have any routes for this object at all.
+            try:
+                names = self.fmap[type(obj)]
+            except KeyError:
+                return (obj, todo)
+
+            routebits = names.get(todo[0])
+
+            if routebits is not None:
+                route, argc = routebits
+                # The first path segment is the route identifier, so we skip
+                # it when identifying arguments.
+                lastseg = todo[0]
+                args = todo[1:argc + 1]
+                todo = todo[argc + 1:]
+            else:
+                # Attempt traversal directly (with no intermediate segment)
+                # as a last resort.
+                if None in names:
+                    route, argc = names[None]
+                    lastseg = None
+                    args = todo[:argc]
+                    todo = todo[argc:]
+                else:
+                    raise NotFound(obj, todo[0])
+
+            if len(args) != argc:
+                # There were too few path segments left. Die.
+                raise InsufficientPathSegments(obj, lastseg, len(args))
+
+            obj = route(obj, *args)
+        return (obj, ())
+
---
+++ ivle/webapp/urls/test_router.py
@@ -0,0 +1,231 @@
+from ivle.webapp.urls import (InsufficientPathSegments, NoPath, NotFound,
+                              RouteConflict, Router, ROOT)
+
+class Root(object):
+    def __init__(self):
+        self.subjects = {}
+
+    def add_subject(self, subject):
+        self.subjects[subject.name] = subject
+
+class Subject(object):
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+        self.offerings = {}
+
+    def add_offering(self, offering):
+        assert self.name == offering.subject.name
+        self.offerings[(offering.year, offering.semester)] = offering
+
+class Offering(object):
+    def __init__(self, subject, year, semester):
+        self.subject = subject
+        self.year = year
+        self.semester = semester
+
+class View(object):
+    def __init__(self, context):
+        self.context = context
+
+class RootIndex(View):
+    pass
+
+class SubjectIndex(View):
+    pass
+
+class SubjectEdit(View):
+    pass
+
+class OfferingIndex(View):
+    pass
+
+class OfferingEdit(View):
+    pass
+
+
+def root_to_subject(root, name):
+    try:
+        return root.subjects[name]
+    except KeyError:
+        raise NotFound(root, name)
+
+def subject_to_offering(subject, year, semester):
+    try:
+        return subject.offerings[(int(year), int(semester))]
+    except (KeyError, ValueError):
+        raise NotFound(subject, (year, semester))
+
+
+def subject_url(subject):
+    return (ROOT, subject.name)
+
+def offering_url(offering):
+    return (offering.subject, (str(offering.year), str(offering.semester)))
+
+
+def subject_to_edit_view(subject):
+    return SubjectEdit(subject)
+
+def subject_to_index_view(subject):
+    return SubjectIndex(subject)
+
+def offering_to_index_view(offering):
+    return OfferingIndex(offering)
+
+def subject_edit_url(view):
+    return (view.context, '+edit')
+
+def subject_index_url(view):
+    return (view.context, '+index')
+
+def offering_index_url(view):
+    return (view.context, '+index')
+
+
+class BaseTest(object):
+    def setUp(self):
+        r = Root()
+        self.r = r
+
+        # Give us some subjects...
+        r.add_subject(Subject('info1', '600151'))
+        r.add_subject(Subject('info2', '600152'))
+        r.add_subject(Subject('info3', '600251'))
+
+        # ... and some offerings.
+        r.subjects['info1'].add_offering(Offering(self.r.subjects['info1'],
+                                         2008, 1))
+        r.subjects['info1'].add_offering(Offering(self.r.subjects['info1'],
+                                         2008, 2))
+        r.subjects['info1'].add_offering(Offering(self.r.subjects['info1'],
+                                         2009, 1))
+        r.subjects['info2'].add_offering(Offering(self.r.subjects['info2'],
+                                         2008, 2))
+        r.subjects['info2'].add_offering(Offering(self.r.subjects['info2'],
+                                         2009, 1))
+        r.subjects['info3'].add_offering(Offering(self.r.subjects['info3'],
+                                         2009, 1))
+
+class TestResolution(BaseTest):
+    def testOneLevel(self):
+        router = Router(root=self.r)
+        router.add_forward(Root, None, root_to_subject, 1)
+
+        assert router.resolve('/info1') is self.r.subjects['info1']
+        assert router.resolve('/info3') is self.r.subjects['info3']
+
+    def testTwoLevels(self):
+        router = Router(root=self.r)
+        router.add_forward(Root, None, root_to_subject, 1)
+        router.add_forward(Subject, None, subject_to_offering, 2)
+
+        assert router.resolve('/info1/2009/1') is \
+               self.r.subjects['info1'].offerings[(2009, 1)]
+        assert router.resolve('/info2/2008/2') is \
+               self.r.subjects['info2'].offerings[(2008, 2)]
+
+    def testDefaultRoute(self):
+        router = Router(root=self.r)
+        router.add_forward(Root, None, root_to_subject, 1)
+        router.add_forward(Subject, None, subject_to_offering, 2)
+        router.add_forward(Subject, '+index', subject_to_index_view, 0)
+        router.add_forward(Subject, '+edit', subject_to_edit_view, 0)
+        router.add_forward(Offering, '+index', offering_to_index_view, 0)
+
+        assert type(router.resolve('/info1')) is \
+               SubjectIndex
+        assert router.resolve('/info1').context is \
+               self.r.subjects['info1']
+
+        assert type(router.resolve('/info1/+edit')) is \
+               SubjectEdit
+        assert router.resolve('/info1/+edit').context is \
+               self.r.subjects['info1']
+
+        assert type(router.resolve('/info1/2009/1')) is \
+               OfferingIndex
+        assert router.resolve('/info1/2009/1').context is \
+               self.r.subjects['info1'].offerings[(2009, 1)]
+
+class TestGeneration(BaseTest):
+    def testOneLevel(self):
+        router = Router(root=self.r)
+        router.add_reverse(Subject, subject_url)
+
+        assert router.generate(self.r.subjects['info1']) == '/info1'
+
+    def testTwoLevel(self):
+        router = Router(root=self.r)
+        router.add_reverse(Subject, subject_url)
+        router.add_reverse(Offering, offering_url)
+
+        assert router.generate(self.r.subjects['info1'].offerings[(2009, 1)]) \
+               == '/info1/2009/1'
+        assert router.generate(self.r.subjects['info2'].offerings[(2008, 2)]) \
+               == '/info2/2008/2'
+
+    def testDefaultRoute(self):
+        router = Router(root=self.r)
+        router.add_reverse(Subject, subject_url)
+        router.add_reverse(Offering, offering_url)
+        router.add_reverse(SubjectIndex, subject_index_url)
+        router.add_reverse(SubjectEdit, subject_edit_url)
+        router.add_reverse(OfferingIndex, offering_index_url)
+
+        assert router.generate(SubjectIndex(self.r.subjects['info1'])) \
+               == '/info1'
+        assert router.generate(SubjectEdit(self.r.subjects['info1'])) \
+               == '/info1/+edit'
+        assert router.generate(OfferingIndex(
+                 self.r.subjects['info1'].offerings[(2009, 1)])) \
+                 == '/info1/2009/1'
+
+class TestErrors(BaseTest):
+    def testForwardConflict(self):
+        router = Router(root=self.r)
+        router.add_forward(Subject, 'foo', object(), 1)
+        try:
+            router.add_forward(Subject, 'foo', object(), 2)
+        except RouteConflict:
+            pass
+        else:
+            raise AssertionError("did not raise RouteConflict")
+
+    def testReverseConflict(self):
+        router = Router(root=self.r)
+        router.add_reverse(Subject, object())
+        try:
+            router.add_reverse(Subject, object())
+        except RouteConflict:
+            pass
+        else:
+            raise AssertionError("did not raise RouteConflict")
+
+    def testNoPath(self):
+        try:
+            Router(root=self.r).generate(object())
+        except NoPath:
+            pass
+        else:
+            raise AssertionError("did not raise NoPath")
+
+    def testNotFound(self):
+        router = Router(root=self.r)
+        router.add_forward(Root, 'foo', object(), 0)
+        try:
+            router.resolve('/bar')
+        except NotFound:
+            pass
+        else:
+            raise AssertionError("did not raise NotFound")
+
+    def testInsufficientPathSegments(self):
+        router = Router(root=self.r)
+        router.add_forward(Root, 'foo', object(), 2)
+        try:
+            router.resolve('/foo/bar')
+        except InsufficientPathSegments:
+            pass
+        else:
+            raise AssertionError("did not raise InsufficientPathSegments")
