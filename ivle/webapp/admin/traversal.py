a3db1846c423289ce713ebf21dc545d74df635e7
William Grant <grantw@unimelb.edu.au>
2009-07-05 20:16:49 +1000

    Fix a missing Storm import which was breaking project traversal.

--- ivle/webapp/admin/traversal.py
+++ ivle/webapp/admin/traversal.py
@@ -15,6 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+from storm.locals import Store
+
 from ivle.database import Offering, ProjectSet, Project, Subject, User
 
 from ivle.webapp import ApplicationRoot
