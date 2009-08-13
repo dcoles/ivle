ac65281dece1e9ea0444ad8cd4dc77ed5a311668
David Coles <coles.david@gmail.com>
2009-08-13 15:15:17 +1000

    Let the publisher generate help entry URLs for us.

--- ivle/webapp/help/__init__.py
+++ ivle/webapp/help/__init__.py
@@ -7,10 +7,11 @@ from ivle.webapp.base.plugins import ViewPlugin, MediaPlugin
 from ivle.webapp.base.xhtml import XHTMLView
 from ivle.webapp.errors import NotFound, Forbidden
 from ivle.webapp.publisher.decorators import forward_route, reverse_route
+from ivle.webapp.publisher import ROOT
 from ivle.webapp import ApplicationRoot
 
-def generate_toc(plugins, req):
-    toc = {}
+def generate_toc(plugins):
+    toc = HelpTree(None, None, {})
     for plugin in plugins:
         if hasattr(plugin, 'help'):
             # Get the dir the plugin resides in
@@ -20,12 +21,12 @@ def generate_toc(plugins, req):
 
 def add_dict(newdict, curdict, plugin):
     for key in curdict:
-        if key not in newdict:
-            newdict[key] = {}
         if isinstance(curdict[key], dict):
+            if key not in newdict:
+                newdict[key] = HelpTree(newdict, key, {})
             add_dict(newdict[key], curdict[key], plugin)
         else:
-            newdict[key] = os.path.join(plugin, curdict[key])
+            newdict[key] = HelpEntry(newdict, key, os.path.join(plugin, curdict[key]))
     return newdict
 
 class HelpTreeView(XHTMLView):
@@ -39,7 +40,10 @@ class HelpTreeView(XHTMLView):
     def populate(self, req, ctx):
         self.plugin_styles[Plugin] = ['help.css']
 
-        ctx['toc'] = self.context.tree
+        ctx['req'] = req
+        ctx['context'] = self.context
+        ctx['HelpTree'] = HelpTree
+        ctx['HelpEntry'] = HelpEntry
 
 class HelpEntryView(XHTMLView):
     tab = 'help'
@@ -51,36 +55,48 @@ class HelpEntryView(XHTMLView):
 
     def populate(self, req, ctx):
         self.plugin_styles[Plugin] = ['help.css']
-
+        
         ctx['helpfile'] = self.context.file
 
-class HelpTree(object):
-    def __init__(self, tree):
-        self.tree = tree
+class HelpTree(dict):
+    def __init__(self, parent, name, tree):
+        super(HelpTree, self).__init__(tree)
+        self.parent = parent
+        self.name = name
 
 class HelpEntry(object):
-    def __init__(self, file):
+    def __init__(self, parent, name, file):
+        self.parent = parent
+        self.name = name
         self.file = file
 
 @forward_route(ApplicationRoot, '+help')
 def root_to_helptree(root):
-    return HelpTree(generate_toc(root.config.plugin_index[ViewPlugin], None))
+    return HelpTree(None, None, generate_toc(root.config.plugin_index[ViewPlugin]))
 
 @forward_route(HelpTree, argc=1)
 def helptree_to_help(help, subhelp_name):
     try:
-        sub = help.tree[subhelp_name]
-        if type(sub) is dict:
-            return HelpTree(sub)
-        else:
-            return HelpEntry(sub)
+        return help[subhelp_name]
     except KeyError:
         # No help entry of that name
         return None
 
+@reverse_route(HelpTree)
+def helptree_url(helptree):
+    if helptree.parent is None:
+        return (ROOT, '+help')
+    return (helptree.parent, helptree.name)
+
+@reverse_route(HelpEntry)
+def helpentry_url(helpentry):
+    return (helpentry.parent, helpentry.name)
+
 class Plugin(ViewPlugin, MediaPlugin):
     """The plugin for viewing help files."""
     forward_routes = (root_to_helptree, helptree_to_help)
+    reverse_routes = (helptree_url, helpentry_url)
+
     views = [(HelpTree, '+index', HelpTreeView),
              (HelpEntry, '+index', HelpEntryView)]
 
