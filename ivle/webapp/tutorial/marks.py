660eb9bcd5015e6d3232265ab67a47fbe19682f5
Matt Giuca <matt.giuca@gmail.com>
2010-02-24 19:33:38 +1100

    tutorial.marks: Added the body of the table, showing all of the fields including worksheet marks.

--- ivle/webapp/tutorial/marks.py
+++ ivle/webapp/tutorial/marks.py
@@ -24,6 +24,8 @@ Displays students' worksheet marks to users with sufficient privileges.
 
 import datetime
 
+import ivle.database
+import ivle.worksheet.utils
 from ivle.webapp.base.xhtml import XHTMLView
 from ivle.webapp.media import media_url
 
@@ -35,7 +37,8 @@ class WorksheetsMarksView(XHTMLView):
 
     def populate(self, req, ctx):
         error = None
-        ctx['context'] = self.context
+        offering = self.context
+        ctx['context'] = offering
 
         # User may supply a "cutoff date" to calculate marks as of that date
         # Default to current time
@@ -51,3 +54,51 @@ class WorksheetsMarksView(XHTMLView):
                         % data.get('cutoff'))
         ctx['cutoff'] = cutoff
         ctx['error'] = error
+
+        # "worksheets" is a list of worksheet names
+        worksheets = offering.worksheets.find(assessable=True)
+        ctx['worksheets'] = [ws.name for ws in worksheets]
+
+        # "students" is a list of tuples:
+        # (user, worksheet_pcts, total_pct, mark)
+        # user is a User object, worksheet_pcts is a list of floats (one per
+        # worksheet), total_pct is a float, mark is an int
+        ctx['students'] = students = []
+        # Get all users enrolled in this offering
+        users = req.store.find(ivle.database.User,
+                       ivle.database.User.id == ivle.database.Enrolment.user_id,
+                       offering.id == ivle.database.Enrolment.offering).order_by(
+                            ivle.database.User.login)
+        for user in users:
+            worksheet_pcts, total_pct, mark = get_marks_user(req, worksheets,
+                                                user, as_of=cutoff)
+            students.append((user, worksheet_pcts, total_pct, mark))
+
+def get_marks_user(req, worksheets, user, as_of=None):
+    """Gets marks for a particular user for a particular set of worksheets.
+    @param worksheets: List of Worksheet objects to get marks for.
+    @param user: User to get marks for.
+    @param as_of: Optional datetime. If supplied, gets the marks as of as_of.
+    @returns: (worksheet_pcts, total_pct, mark)
+    """
+    worksheet_pcts = []
+    # As we go, calculate the total score for this subject
+    # (Assessable worksheets only, mandatory problems only)
+    problems_done = 0
+    problems_total = 0
+
+    for worksheet in worksheets:
+        # We simply ignore optional exercises here
+        mand_done, mand_total, _, _ = (
+            ivle.worksheet.utils.calculate_score(req.store, user, worksheet,
+                                                 as_of))
+        if mand_total > 0:
+            worksheet_pcts.append(float(mand_done) / mand_total)
+        else:
+            # Avoid Div0, just give everyone 0 marks if there are none
+            worksheet_pcts.append(0.0)
+        problems_done += mand_done
+        problems_total += mand_total
+    percent, mark, _ = (
+        ivle.worksheet.utils.calculate_mark(problems_done, problems_total))
+    return (worksheet_pcts, float(percent)/100, mark)
