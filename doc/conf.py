d58a0aefd504ff96cefeaf29679bbb59b12ea84a
William Grant <grantw@unimelb.edu.au>
2010-02-25 22:55:55 +1100

    Update version strings for 1.0rc1.

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,7 @@ copyright = '2009-2010, University of Melbourne'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0beta2'
+release = '1.0rc1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
