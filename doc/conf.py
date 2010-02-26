eef4b4faf9a8f90cd1b78cb9f9e6cc54342bad35
William Grant <grantw@unimelb.edu.au>
2010-02-27 00:27:55 +1100

    Bump version to 1.0. This is it.

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,7 @@ copyright = '2009-2010, University of Melbourne'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0rc2'
+release = '1.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
