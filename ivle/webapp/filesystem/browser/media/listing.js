fb7d4f5c48b6bfa9be80dd367a653323b4ce3ba3
David Coles <coles.david@gmail.com>
2010-06-02 23:11:45 +1000

    File Browser: IE does not trigger "onchange" with checkboxes until "blur". Use "onclick" to get immediate feedback (and behave consistantly across browsers)

--- ivle/webapp/filesystem/browser/media/listing.js
+++ ivle/webapp/filesystem/browser/media/listing.js
@@ -634,7 +634,7 @@ function setup_dir_listing()
 <thead> \
   <tr class="rowhead"> \
     <th class="col-check"> \
-      <input title="Select All" onchange="action_selectall(this.checked)" type="checkbox"> \
+      <input title="Select All" onclick="action_selectall(this.checked)" type="checkbox"> \
     </th> \
     <th colspan="3" class="col-filename"> \
       <a onclick="return sort_listing(\'filename\', !sort_ascending)" title="Sort by filename" href="">Filename</a> \
@@ -711,7 +711,7 @@ function handle_dir_listing(path, listing)
         checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
         checkbox.setAttribute("title", "Select this file");
-        checkbox.setAttribute("onchange", "update_selection()");
+        checkbox.setAttribute("onclick", "update_selection()");
         /* Check the box if selected_files says it's selected */
         checkbox.checked = filename in sel_files_dict;
         td.appendChild(checkbox);
